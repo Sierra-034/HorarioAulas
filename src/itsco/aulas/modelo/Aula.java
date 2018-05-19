699667b876fc4a555aa40503996aa9f4f7894089
Samuel Gomez Balderas <sierra_034@outlook.com>
2018-05-19 15:06:15 -0500

    Modelo Aula creado

---
+++ src/itsco/aulas/modelo/Aula.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package itsco.modelo;
+
+/**
+ *
+ * @author Samuel Gomez
+ */
+public class Aula {
+    
+    private Integer idAula;
+    private Integer numeroSillas;
+    
+    public Aula(int nSillas) {
+        numeroSillas = nSillas;
+    }
+
+    public Integer getIdAula() {
+        return idAula;
+    }
+
+    public void setIdAula(Integer idAula) {
+        this.idAula = idAula;
+    }
+
+    public Integer getNumeroSillas() {
+        return numeroSillas;
+    }
+
+    public void setNumeroSillas(Integer numeroSillas) {
+        this.numeroSillas = numeroSillas;
+    }
+
+    @Override
+    public String toString() {
+        String str = String.format("[%d, %d]", idAula, numeroSillas);
+        return str;
+    }
+    
+}
